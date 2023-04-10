acd9c42035a8163f296e5447c13fb05ad6eadcfa
rxd0705 <84168080+rxd0705@users.noreply.github.com>
2023-04-10 14:40:57 +0900

    [Bronze V] Title: 개, Time: 196 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //문제
+        //아래 예제와 같이 개를 출력하시오.
+        //
+        //입력
+        //없음.
+        //
+        //출력
+        //개를 출력한다.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+
+    }
+}
