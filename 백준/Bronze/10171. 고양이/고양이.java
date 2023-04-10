b47e5192c8a779b4988007d93b9edd86666251d2
rxd0705 <84168080+rxd0705@users.noreply.github.com>
2023-04-10 14:39:41 +0900

    [Bronze V] Title: 고양이, Time: 196 ms, Memory: 17364 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //문제
+        //아래 예제와 같이 고양이를 출력하시오.
+        //
+        //입력
+        //없음.
+        //
+        //출력
+        //고양이를 출력한다.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
